afa6858c87d6f0c149eecf5b25e179189945a2c7
Hristo Genev <harrygg@gmail.com>
2016-03-20 15:48:22 +0200

    dummy changes

--- plugin.video.free.bgtvs/addon.py
+++ plugin.video.free.bgtvs/addon.py
@@ -4,16 +4,21 @@ from resources.lib.helper import *
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
-
 params = get_params()
 
-try: id = int(params["id"])
-except: id = None
+try: 
+	id = int(params["id"])
+except: 
+	id = None
 
-try: mode = params["mode"]
-except: mode = None
-	
-if mode == None: show_channels()
-else: show_streams(id)
+try: 
+	mode = params["mode"]
+except: 
+	mode = None
+
+if mode == None: 
+	show_channels()
+else: 
+	show_streams(id)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
